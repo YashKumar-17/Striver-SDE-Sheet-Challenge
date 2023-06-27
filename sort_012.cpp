4a53d190a6c6dd709e8f3214c86d486ad70a0fe7
Yash Kumar <90023077+YashKumar-17@users.noreply.github.com>
2023-06-27 22:31:58 +0530

    Update sort_012.cpp

--- sort_012.cpp
+++ sort_012.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h> 
 void sort012(int *arr, int n)
 {
-   sort(arr,arr+n);
+   int l=0;
+   int m=0;
+   int h=n-1;
+   while(m<=h){
+      switch(arr[m]){
+         case 0:
+             swap(arr[l++],arr[m++]);
+             break;
+         case 1:
+             m++;
+             break;
+         case 2:
+             swap(arr[m],arr[h--]);
+             break;
+      }
+   }
 }
